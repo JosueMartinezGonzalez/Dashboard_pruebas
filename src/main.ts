1a2ccd652dbccdb5c7e043754be9123ce035c539
Josué Martinez <48193044+JosueMartinezGonzalez@users.noreply.github.com>
2023-02-04 19:06:06 -0500

    Inicio 7

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
       },
     }),
   );
+  app.use(cors({
+    origin: '*',
+  }));
   app.enableCors();
 
   const config = new DocumentBuilder()

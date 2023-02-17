dfabc25cdf11a635c33a1717e76610b8508abb80
Josué Martinez <48193044+JosueMartinezGonzalez@users.noreply.github.com>
2023-02-17 12:05:55 -0500

    Nuevas pruebas

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -3,8 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { VideoController } from './controllers/videos.controller';
 import { VideoService } from './services/videos.service';
-import { Video, VideoSchema } from './entities/videos.entity'
-
+import { Video, VideoSchema } from './entities/videos.entity';
 
 @Module({
   imports: [

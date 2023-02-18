dcd5ab0136b6073848e920f941597d56c27f1670
Josué Martinez <48193044+JosueMartinezGonzalez@users.noreply.github.com>
2023-02-18 14:00:52 -0500

    Power mind

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+// import * as cors from 'cors';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

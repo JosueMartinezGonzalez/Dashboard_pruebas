b83ad40f73d385b242741f32ac7c4ed0bee18c20
Josué Martinez <48193044+JosueMartinezGonzalez@users.noreply.github.com>
2023-02-20 06:44:01 -0500

    ¨Powermind Cors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
@@ -9,7 +9,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from './../users/users.module';
 import { AuthController } from './controllers/auth.controller';
 import config from './../config';
-
+import * as bodyParser from 'body-parser';
 @Module({
   imports: [
     UsersModule,
@@ -29,4 +29,20 @@ import config from './../config';
   providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With',
+      );
+      if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+      } else {
+        next();
+      }
+    });
+  }
+}
